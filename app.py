806d9ddcc6d49ee6d167a96217a6d5af720b7428
MoizAhmad924 <141162811+MoizAhmad924@users.noreply.github.com>
2025-01-10 11:01:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -37,24 +37,6 @@ with col5:
 city = st.selectbox('Select city', sorted(cities), key='city')
 last_five = st.number_input('Runs scored in last 5 overs', key='last_five')
 
-#(col1, col2 = st.beta_columns(2)
-
-#with col1:
-    #batting_team = st.selectbox('Select batting team',sorted(teams))
-#with col2:
-    #bowling_team = st.selectbox('Select bowling team', sorted(teams))
-
-#city = st.selectbox('Select city',sorted(cities))
-
-#col3,col4,col5 = st.beta_columns(3)
-
-#with col3:
-    #current_score = st.number_input('Current Score')
-#with col4:
-    #overs = st.number_input('Overs done(works for over>5)')
-#with col5:
-    #wickets = st.number_input('Wickets out')
-
 if st.button('Predict Score'):
     balls_left = 120 - (overs*6)
     wickets_left = 10 -wickets
